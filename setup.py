e3fdadafde1df970a0337357a35464376a7740ef
Ananya Kumar <skywalker94@gmail.com>
2021-04-11 13:29:56 -0700

    Update library version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uncertainty-calibration",
-    version="0.0.8",
+    version="0.0.9",
     author="Ananya Kumar",
     author_email="skywalker94@gmail.com",
     description="Utilities to calibrate model uncertainties and measure calibration.",
